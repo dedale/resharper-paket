0153f9b23f2820be16e928c12acbab3803e1abe3
dedale <ded@laposte.net>
2015-10-02 23:50:53 +0200

    Init ProjectScaffold

---
+++ src/Resharper.Paket/AssemblyInfo.fs
@@ -0,0 +1,12 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("Resharper.Paket")>]
+[<assembly: AssemblyProductAttribute("Resharper.Paket")>]
+[<assembly: AssemblyDescriptionAttribute("Plugin for ReSharper to support Paket references correctly")>]
+[<assembly: AssemblyVersionAttribute("1.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.0")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "1.0"
---
+++ tests/Resharper.Paket.Tests/Tests.fs
@@ -0,0 +1,10 @@
+module Resharper.Paket.Tests
+
+open Resharper.Paket
+open NUnit.Framework
+
+[<Test>]
+let ``hello returns 42`` () =
+  let result = Library.hello 42
+  printfn "%i" result
+  Assert.AreEqual(42,result)
